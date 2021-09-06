dffa23d3e8fd408abf96ead0a9424534fd150d86
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-09-06 20:32:17 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import countryMarkup from './tamplates/country.hbs'
+import countryMarkup from './tamplates/country.hbs';
+import { debounce } from 'lodash';
 
 const inputRef = document.querySelector('.js_input');
 const listRef = document.querySelector('.js_list');
 
-inputRef.addEventListener('input', _.debounce(onInput, 500));
+inputRef.addEventListener('input', debounce(onInput, 500));
+
 
 function onInput(e) {
-  const userCountry = e.currentTarget.value;
-  fetch(`https://restcountries.eu/rest/v2/name/${userCountry}`).then(res => {
-    console.log(res);
-  });
+  const userCountry = e.target.value;
+  fetch('https://restcountries.eu/rest/v2/name')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      listRef.innerHTML = countryMarkup(data);
+    });
 }
