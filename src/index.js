87381427b241d2bb3654fe8925cbbee0e01ed020
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-09-07 16:16:20 +0300

    add full hw

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import countryMarkup from './tamplates/country.hbs';
+import countriesMarkup from './tamplates/countries.hbs';
+import { alert } from '@pnotify/core';
 import { debounce } from 'lodash';
 
 const inputRef = document.querySelector('.js_input');
 const listRef = document.querySelector('.js_list');
+const markup = document.querySelector('.js_markup');
 
 inputRef.addEventListener('input', debounce(onInput, 500));
 
-
 function onInput(e) {
   const userCountry = e.target.value;
-  fetch('https://restcountries.eu/rest/v2/name')
+  console.log(userCountry);
+  if (userCountry) {
+    listRef.innerHTML = '';
+    markup.innerHTML = '';
+    
+  }
+  fetch(`https://restcountries.eu/rest/v2/name/${userCountry}`)
     .then(res => res.json())
     .then(data => {
       console.log(data);
-      listRef.innerHTML = countryMarkup(data);
+      if (data.length === 1) {
+        return (markup.innerHTML = countryMarkup(data[0]));
+      }
+
+      if (data.length > 1 && data.length < 10) {
+        return (listRef.innerHTML = countriesMarkup(data));
+      }
+      if (data.length > 10) {
+        return alert({
+          text: 'To many matches found. Please enter more specific query!',
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
     });
 }
